18b6812fa1c2611d5b2568c78edbc6b65a7bac6f
Bakujon <john.chapon@gmail.com>
2023-03-14 14:37:07 +0100

    a

---
+++ Model/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+class Config
+{
+    const DBHOST = 'localhost';
+    const DBNAME = 'company';
+    const DBUSER = 'root';
+    const DBPWD = '';
+    const DBCHARSET = 'utf8mb4';
+}
--- Public/assets/js/script.js
+++ Public/assets/js/script.js
@@ -1,1 +1,19 @@
-console.log("Coucou");
+function doRecherche() {
+  let data = new FormData(document.getElementById("maRecherche"));
+
+  fetch("http://localhost/php_mvc/Controller/Controller.php", {
+    method: "POST",
+    body: data,
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      let resultats = document.getElementById("resultatDiv");
+      if (res !== null) {
+        for (let r of res) {
+          resultats.innerHTML += `<div>${r.employee_id} - ${r.first_name} - ${r.last_name}</div>`;
+        }
+      }
+    });
+  return false;
+}
